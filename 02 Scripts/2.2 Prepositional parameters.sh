bc8c95bc174ae42a97bf6c9796a51f985a7b22a4
Shubhangi Dhongade <106398285+Shubhangi072@users.noreply.github.com>
2023-01-27 23:28:36 +0530

    Create 2.2 Prepositional parameters.sh

---
+++ 02 Scripts/2.2 Prepositional parameters.sh
@@ -0,0 +1,8 @@
+echo "Printing name of shell script" $0
+
+echo "1st parameter" $1
+echo "2nd parameter" $2
+echo "Total parameter" $#
+echo "All parameter" $*
+
+echo "Is previous command executed?" $?
